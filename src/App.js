0d319e4ee4143b38b2dbf82b9371f5da9e7e946f
jaygraffagna <jaygraffagna10@gmail.com>
2019-07-17 22:12:49 -0500

    add resume

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ const bgStyle = {
   backgroundPosition: "center", /* Center the image */
   backgroundRepeat: "no-repeat", /* Do not repeat the image */
   backgroundSize: "cover",
+  fontFamily: "sans-serif",
+  margin: "0px"
 }
 
 const hdrStyle = {
-  paddingTop: "60px",
+  color:"white",
+  paddingTop: "65px",
   textAlign: "center",
-  fontSize: "48px",
-  fontWeight: "500"
+  fontSize: "64px",
+  fontWeight: "300"
 }
 
 const prfStyle = {
-  marginTop:"15px",
+  paddingTop:"15px",
   height:"300px",
   width:"300px",  
   borderRadius: "50%",
@@ -30,10 +33,29 @@ const prfStyle = {
 
 const descStyle = {
   marginTop:"20px",
-  backgroundColor: "white",
+  color: "white",
   fontSize:"20px",
-  borderRadius:"10px",
+  borderRadius:"5px",
   textAlign:"center",
+  width:"500px",
+  display:"block",
+  marginLeft:"auto",
+  marginRight:"auto",
+  padding:"10px"
+}
+
+const parntStyle = {
+  backgroundColor: "black",
+  padding: "0px",
+  height: "100%"
+}
+
+const resumStyle = {
+  marginTop:"20px",
+  backgroundColor: "#494949",
+  color: "white",
+  fontSize:"12px",
+  borderRadius:"5px",
   width:"500px",
   display:"block",
   marginLeft:"auto",
@@ -43,17 +65,64 @@ const descStyle = {
 
 function App() {
   return (
-    <div style={bgStyle}  >
-      <header style={hdrStyle}>
-        Jay Graffagna
-      </header>
-      <img src={prf} alt="profile" style={prfStyle} />
+    <div style={parntStyle}>
+      <div style={bgStyle}  >
+        <img src={prf} alt="profile" style={prfStyle} />
+        <header style={hdrStyle}>
+          Jay Graffagna
+        </header>
+        <p style={descStyle}>
+          I am a developer from Iowa working at MidAmerican Energy.  
+          I care about people and the planet.  
+          I post some code <a href="https://github.com/jaygraffagna">here</a>.  
+          I can be reached via email <a href="mailto:jaygraffagna10@gmail.com">here</a>.
+        </p>
+      </div>
+      <p style={resumStyle}>
+        /* Resume */ <br/>
+      
+        Education: Augustana College - B.A. Computer Science & Applied Mathematics<br/>
+        GPA: 3.5<br/><br/>
 
-
-      <p style={descStyle}>
-        I am a developer from Iowa working at MidAmerican Energy.  I care about the people and the planet.  I post some code here.  I can be reached via email here.
+        Languages: C#, Oracle SQL, Javascript, HTML, CSS, Java<br/><br/>
+          
+        Technologies:  Asp.Net, Xamarin, SignalR, WPF, React, Node, Android, JSON, XML<br/><br/>
+          
+        Tools: Visual Studio, PL/SQL Developer, VS Code, Eclipse, Microsoft TFS, Git, Splunk<br/><br/>
+          
+        Employement<br/><br/>
+          
+        Software Developer II - MidAmerican Energy Davenport, IA<br/>
+        * Lead projects from design, development, & deployment to meet business requirements<br/>
+        * Defined standards within an internal team used across the entire development department<br/>
+        * Guided team members & contractors throughout the development process<br/>
+        * Researched new technologies to provide management recommendations for future projects<br/><br/>
+          
+        Software Developer I - MidAmerican Energy Davenport, IA<br/>
+        * Contributed to development projects defined by the business<br/>
+        * Provided support within 24/7 on-call rotation<br/>
+        * Debugged existing applications to resolve user issues<br/>
+        * Provided assistants to gather project requirements<br/><br/>
+          
+        Software Developer Intern - MidAmerican Energy Davenport, IA<br/>
+        * Learned the software development lifecycle with an enteprise<br/>
+        * Communicated accross business team to understand development requirements<br/>
+        * Assisted in building modules within software development projects<br/>
+        * Branched code bases to divide developemnt work<br/><br/>
+          
+        Skills<br/>
+        * Strong Communicator<br/>
+        * Project Leadership<br/>
+        * Task Planning<br/>
+        * Detail Oriented<br/>
+        * High Adaptive<br/>
+        * Positive Minded<br/><br/>
+          
+        Attributes<br/>
+        * Bettendorf Natural Resouces Commitee Member<br/>
+        * Living Lands & Waters Volunteer<br/>
+        * Augustana Varsity Track Captain<br/>
       </p>
-    
     </div>
   );
 }
